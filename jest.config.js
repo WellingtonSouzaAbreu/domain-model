9d65d91e805cef79f6f2c16760d23fade94d9697
Wellington Souza <wellingtonsouza6300@gmail.com>
2024-04-24 08:55:26 -0400

    chore: Adiciona comentário de testes

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     preset: 'ts-jest',
 	testEnvironment: 'node',
 	testMatch: ["**/test/*.test.ts"],
-	collectCoverage: false,
+	collectCoverage: false, // Deixar true para visualizar % de cobertura de testes
 	moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" }
 }
